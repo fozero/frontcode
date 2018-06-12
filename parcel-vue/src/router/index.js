640d29c78d96895bde53d926c40ece3b5e149498
wuj <wuj@yindou.com>
2018-06-12 18:40:00 +0800

    test

--- parcel-vue/src/router/index.js
+++ parcel-vue/src/router/index.js
@@ -3,13 +3,15 @@ import Router from 'vue-router'
 // 全加载
 // import Index from '../components/Index'
 // 懒加载
-const Index = () => import('../components/Index')
+// const Index = () => import('../components/Index')
+// const Index = () => import('../views/Index')
+const app = () => import('../test/app')
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
     path: '/',
-    component: Index
+    component: app
   }]
 })
---
+++ parcel-vue/src/views/Index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="index">
+        
+    </div>
+</template>
+<script>
+export default {
+  name: "index",
+  mounted() {},
+  data() {
+    return {
+      showTip: false
+    };
+  },
+  methods: {
+    showClick() {
+      this.showTip = true;
+    }
+  }
+};
+</script>
+<style scoped>
+
+</style>
