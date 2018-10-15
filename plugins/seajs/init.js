e08ab602ad3152d269738eb7c5e5183ca0782cf7
wujian <wuj3@xiaopeng.com>
2018-10-15 20:39:52 +0800

    update

--- plugins/seajs/init.js
+++ plugins/seajs/init.js
@@ -2,13 +2,15 @@
 * @Author: fozero@126.
 * @Date:   2018-10-11 18:41:55
 * @Last Modified by:   fozero
-* @Last Modified time: 2018-10-11 23:38:05
+* @Last Modified time: 2018-10-15 11:28:45
 */
 define(function(require, exports, module){
   
+	var $ = require('jquery');
 	var m1=require('./js/M1');
-	  
+	console.log('txt',$('.txt'))
 	exports.init=function(){
 		m1.run();
+		// console.log('txt',$('.txt'))
 	}
 });
