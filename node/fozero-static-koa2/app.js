e1eaef8a56bbc1b9df57affbf882834acaf47d57
fozero <fozero@126.com>
2018-11-14 03:09:06 +0800

    update

--- node/fozero-static-koa2/app.js
+++ node/fozero-static-koa2/app.js
@@ -1,27 +1,16 @@
 
 // app.js
-// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
-
 const path = require('path')
-
-// 注意require('koa-router')返回的是函数:
 const router = require('koa-router')();
-
 // 模板引擎
 const render = require('koa-art-template');
-
 // 跨域
 var cors = require('koa2-cors');
-
-
 // body解析
 const bodyParser = require('koa-bodyparser');
-
-
 // 静态资源
 const static = require('koa-static');
-
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
 
@@ -30,22 +19,13 @@ render(app, {
   extname: '.html',
   // debug: process.env.NODE_ENV !== 'production'
 });
-
-// 跨域
 app.use(cors());
-
-
 // 解决body 需在router之前注册到app对象上
 app.use(bodyParser());
-
 // 静态资源目录
 app.use(static(
   path.join( __dirname,'./static')
 ));
-
-
-
-
 
 
 app.use(async (ctx, next) => {
@@ -62,50 +42,15 @@ app.use(async (ctx, next) => {
 
 
 // 对于任何请求，app将调用该异步函数处理请求：
-app.use(async (ctx, next) => {
-    await next();
-    // ctx.response.type = 'text/html';
-    // ctx.response.body = '<h1>Hello, koa2!</h1>';
-});
-
-
-
-// add url-route:
-router.get('/hello/:name', async (ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
+// app.use(async (ctx, next) => {
+//     await next();
+// });
 
 router.get('/user', async (ctx, next) => {
-    // ctx.render('user');
     ctx.render('user',{
-            data:'hello msg'
+            data:'kerry'
     });
 });
-
-
-router.get('/', async (ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>
-        <form action="/signin" method="post">
-            <p>Name333: <input name="name" value="koa"></p>
-            <p>Password: <input name="password" type="password"></p>
-            <p><input type="submit" value="Submit"></p>
-        </form>`;
-});
-
-router.post('/signin', async (ctx, next) => {
-    var
-        name = ctx.request.body.name || '',
-        password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
-    }
-});
-
 
 // add router middleware:
 app.use(router.routes());
