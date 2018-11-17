1ac19a7835086419d43f2791d1f2bd5e89fecc8d
fozero <fozero@126.com>
2018-11-17 23:21:00 +0800

    update

--- plugins/d3/map.js
+++ plugins/d3/map.js
@@ -13,14 +13,14 @@ var mapChart = mapChart || {};
 	}
 }(typeof window !== 'undefined' ? window : this, function (mapChart) {
 	mapChart = mapChart || {};
-	var width=500,height=400;
+	var width=500,height=500;
 	mapChart.init = function () {
 		console.log(11111111)
         var svg = d3.select("#chinaMap")
           .append("svg")
           .attr("width",width)
           .attr("height",height)
-          .style("padding-top","50px");
+          // .style("padding-top","50px");
         // 投影函数  经度纬度转换
         var projection = d3.geo.mercator()
           .center([107,31])
@@ -53,25 +53,45 @@ var mapChart = mapChart || {};
             // 鼠标进入事件
             .on("mouseover",function(d){
               console.log('d',d)
+              var that = this;
                 list.forEach(function(ite,index){
                   if(ite.item==d.properties.name){
+                    d3.select(that).attr("fill","rgb(255, 225, 103)");
                     //设置tooltip文字
-                    tooltip.html('<ul style="margin: 0;padding: 0;width: 200px;height: 100px;background: #fff;border: 1px solid #eee;border-radius: 5px;box-sizing: border-box;"><li class="title" style="background: #eee;display: flex;justify-content: space-between;padding: 5px;">'+d.properties.name+'</li><li style="display: flex;justify-content: space-between;padding: 5px;"><span class="label">订单</span><span class="value">'+ite.percent+'</span></li><li style="display: flex;justify-content: space-between;padding: 5px;"><span class="label">占比</span><span class="value">'+ite.count+'</span></li></ul>')
+                    tooltip.html('<ul style="margin: 0;padding: 0;width: 200px;line-height: 22px;color: #666;font-size: 12px;background: rgba(255,255,255,0.5);box-shadow: 0 0 8px 3px rgba(238,238,238,0.8);border-radius: 5px;box-sizing: border-box;"><li class="title" style="background: rgba(238,238,238,0.5);    border-bottom: 1px solid #eee;display: flex;justify-content: space-between;padding: 5px;">'+d.properties.name+'</li><li style="display: flex;justify-content: space-between;padding: 5px;"><span class="label label1">订单</span><span class="value">'+ite.percent+'</span></li><li style="display: flex;justify-content: space-between;padding: 5px;"><span class="label label2">占比</span><span class="value">'+ite.count+'</span></li></ul>')
                             .style("left",(d3.event.pageX)+"px")
                             .style("top",(d3.event.pageY+20)+"px")
                             .style("opacity",1.0);
                     }
-                    // d3.select(this).attr("fill","rgb(225, 225, 103)");
+                    
                 })
             })
             .on("mouseout",function(d){
                 tooltip.style("opacity",0); 
+              var that = this;
+                list.forEach(function(ite,index){
+                  if(ite.item==d.properties.name){
+                    if(ite.percent>0.4){
+                      d3.select(that).attr("fill","rgb(56, 135, 224)");
+                    }else if(ite.percent>0.2){
+                      d3.select(that).attr("fill","rgb(199, 221, 246)");
+                    }else{
+                      d3.select(that).attr("fill","rgb(235, 243, 252)");
+                    }
+                  }
+                })
             });
           //  设置背景色
           list.forEach(function(ite,index){
             groups.selectAll("path").style("fill",function(d,i){
               if(ite.item==d.properties.name){
-                  d3.select(this).attr("fill","rgb(235, 243, 252)");
+                  if(ite.percent>0.4){
+                    d3.select(this).attr("fill","rgb(56, 135, 224)");
+                  }else if(ite.percent>0.2){
+                    d3.select(this).attr("fill","rgb(199, 221, 246)");
+                  }else{
+                    d3.select(this).attr("fill","rgb(235, 243, 252)");
+                  }
               }
             })
           });
